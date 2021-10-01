0fc7d2f1e336c680cb3d545c73500d8856dd1b96
ashwindevx <ashwindevx@gmail.com>
2021-10-01 23:50:06 +0530

    minor style changes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import React from 'react'
-import Home from './components/Home'
-
+import React from "react";
+import Main from "./components/Main";
 
 function App() {
   return (
     <div>
-      <Home/>
-
+      <Main />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
